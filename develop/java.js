c45abf7487bca8d490412e573e951c0f21b08ca2
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-03-07 11:14:47 -0600

    Added the necessary javascript to return data from a fetch request made to the exchangerate api

--- develop/java.js
+++ develop/java.js
@@ -17,14 +17,19 @@ function convertCurrency() {
     // API KEY CHANGES BASED ON DEVELOPER
     var kanesApiKey = "9470f9dffac1cee9291efaaa";
     var rachelsApiKey = "97450b01cbb760880bc42f8b";
+    // var halimasApiKey =;
+    // var gilbertsApiKey =;
 
     // Placeholder
     var requestUrl = "https://v6.exchangerate-api.com/v6/" + kanesApiKey + "/pair/" + baseCurrency + "/" + conversionCurrency + "/" + currencyAmount;
 
     // Returns data based on the requestUrl
     fetch(requestUrl)
-    .then(function (response) {
-        console.log("response", response)
+    .then((response) => {
         return response.json();
+    })
+
+    .then((data) => {
+        console.log(data)
     })
 }
